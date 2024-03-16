8331f1f50697aad1a6e24972e72583f988f621cd
aobatact <aobatact144@gmail.com>
2024-03-16 18:18:34 +0900

    wip cow fn

--- compact_str/src/cow.rs
+++ compact_str/src/cow.rs
@@ -1,4 +1,5 @@
 use core::hash::{Hash, Hasher};
+use core::ops::RangeBounds;
 use core::str::FromStr;
 use core::{
     borrow::{Borrow, BorrowMut},
@@ -12,7 +13,7 @@ use alloc::boxed::Box;
 use alloc::fmt;
 use alloc::{borrow::Cow, string::String};
 
-use crate::ToCompactString;
+use crate::Drain;
 use crate::{repr::Repr, CompactString, ReserveError, UnwrapWithMsg, Utf16Error};
 
 #[repr(transparent)]
@@ -418,8 +419,159 @@ impl<'a> CompactCowStr<'a> {
     /// ```
     #[inline]
     pub fn as_mut_str(&mut self) -> &mut str {
-        let len = self.len();
-        unsafe { core::str::from_utf8_unchecked_mut(&mut self.0.as_mut_buf()[..len]) }
+        self.as_mut_compact_string().as_mut_str()
+    }
+
+    /// Returns a byte slice of the [`CompactString`]'s contents.
+    ///
+    /// # Examples
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let s = CompactString::new("hello");
+    ///
+    /// assert_eq!(&[104, 101, 108, 108, 111], s.as_bytes());
+    /// ```
+    #[inline]
+    pub fn as_bytes(&self) -> &[u8] {
+        &self.0.as_slice()[..self.len()]
+    }
+
+    // TODO: Implement a `try_as_mut_slice(...)` that will fail if it results in cloning?
+    //
+    /// Provides a mutable reference to the underlying buffer of bytes.
+    ///
+    /// # Safety
+    /// * All Rust strings, including `CompactString`, must be valid UTF-8. The caller must
+    ///   guarantee
+    /// that any modifications made to the underlying buffer are valid UTF-8.
+    ///
+    /// # Examples
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::new("hello");
+    ///
+    /// let slice = unsafe { s.as_mut_bytes() };
+    /// // copy bytes into our string
+    /// slice[5..11].copy_from_slice(" world".as_bytes());
+    /// // set the len of the string
+    /// unsafe { s.set_len(11) };
+    ///
+    /// assert_eq!(s, "hello world");
+    /// ```
+    #[inline]
+    pub unsafe fn as_mut_bytes(&mut self) -> &mut [u8] {
+        self.0.as_mut_buf()
+    }
+
+    /// Appends the given [`char`] to the end of this [`CompactString`].
+    ///
+    /// # Examples
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::new("foo");
+    ///
+    /// s.push('b');
+    /// s.push('a');
+    /// s.push('r');
+    ///
+    /// assert_eq!("foobar", s);
+    /// ```
+    pub fn push(&mut self, ch: char) {
+        self.push_str(ch.encode_utf8(&mut [0; 4]));
+    }
+
+    /// Removes the last character from the [`CompactString`] and returns it.
+    /// Returns `None` if this [`CompactString`] is empty.
+    ///
+    /// # Examples
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::new("abc");
+    ///
+    /// assert_eq!(s.pop(), Some('c'));
+    /// assert_eq!(s.pop(), Some('b'));
+    /// assert_eq!(s.pop(), Some('a'));
+    ///
+    /// assert_eq!(s.pop(), None);
+    /// ```
+    #[inline]
+    pub fn pop(&mut self) -> Option<char> {
+        self.0.pop()
+    }
+
+    /// Appends a given string slice onto the end of this [`CompactString`]
+    ///
+    /// # Examples
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::new("abc");
+    ///
+    /// s.push_str("123");
+    ///
+    /// assert_eq!("abc123", s);
+    /// ```
+    #[inline]
+    pub fn push_str(&mut self, s: &str) {
+        self.0.push_str(s)
+    }
+
+    /// Removes a [`char`] from this [`CompactString`] at a byte position and returns it.
+    ///
+    /// This is an *O*(*n*) operation, as it requires copying every element in the
+    /// buffer.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `idx` is larger than or equal to the [`CompactString`]'s length,
+    /// or if it does not lie on a [`char`] boundary.
+    ///
+    /// # Examples
+    ///
+    /// ### Basic usage:
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut c = CompactString::from("hello world");
+    ///
+    /// assert_eq!(c.remove(0), 'h');
+    /// assert_eq!(c, "ello world");
+    ///
+    /// assert_eq!(c.remove(5), 'w');
+    /// assert_eq!(c, "ello orld");
+    /// ```
+    ///
+    /// ### Past total length:
+    ///
+    /// ```should_panic
+    /// # use compact_str::CompactString;
+    /// let mut c = CompactString::from("hello there!");
+    /// c.remove(100);
+    /// ```
+    ///
+    /// ### Not on char boundary:
+    ///
+    /// ```should_panic
+    /// # use compact_str::CompactString;
+    /// let mut c = CompactString::from("🦄");
+    /// c.remove(1);
+    /// ```
+    #[inline]
+    pub fn remove(&mut self, idx: usize) -> char {
+        self.as_mut_compact_string().remove(idx)
+    }
+
+    /// Forces the length of the [`CompactString`] to `new_len`.
+    ///
+    /// This is a low-level operation that maintains none of the normal invariants for
+    /// `CompactString`. If you want to modify the `CompactString` you should use methods like
+    /// `push`, `push_str` or `pop`.
+    ///
+    /// # Safety
+    /// * `new_len` must be less than or equal to `capacity()`
+    /// * The elements at `old_len..new_len` must be initialized
+    #[inline]
+    pub unsafe fn set_len(&mut self, new_len: usize) {
+        self.0.set_len(new_len)
     }
 
     /// Returns whether or not the [`CompactString`] is heap allocated.
@@ -445,6 +597,335 @@ impl<'a> CompactCowStr<'a> {
         self.0.is_heap_allocated()
     }
 
+    /// Removes the specified range in the [`CompactString`],
+    /// and replaces it with the given string.
+    /// The given string doesn't need to be the same length as the range.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the starting point or end point do not lie on a [`char`]
+    /// boundary, or if they're out of bounds.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::new("Hello, world!");
+    ///
+    /// s.replace_range(7..12, "WORLD");
+    /// assert_eq!(s, "Hello, WORLD!");
+    ///
+    /// s.replace_range(7..=11, "you");
+    /// assert_eq!(s, "Hello, you!");
+    ///
+    /// s.replace_range(5.., "! Is it me you're looking for?");
+    /// assert_eq!(s, "Hello! Is it me you're looking for?");
+    /// ```
+    #[inline]
+    pub fn replace_range(&mut self, range: impl RangeBounds<usize>, replace_with: &str) {
+        self.as_mut_compact_string()
+            .replace_range(range, replace_with)
+    }
+
+    /// Creates a new [`CompactString`] by repeating a string `n` times.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if the capacity would overflow.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// use compact_str::CompactString;
+    /// assert_eq!(CompactString::new("abc").repeat(4), CompactString::new("abcabcabcabc"));
+    /// ```
+    ///
+    /// A panic upon overflow:
+    ///
+    /// ```should_panic
+    /// use compact_str::CompactString;
+    ///
+    /// // this will panic at runtime
+    /// let huge = CompactString::new("0123456789abcdef").repeat(usize::MAX);
+    /// ```
+    #[must_use]
+    pub fn repeat(&self, n: usize) -> Self {
+        self.as_ref_compact_string().repeat(n).into()
+    }
+
+    /// Truncate the [`CompactString`] to a shorter length.
+    ///
+    /// If the length of the [`CompactString`] is less or equal to `new_len`, the call is a no-op.
+    ///
+    /// Calling this function does not change the capacity of the [`CompactString`].
+    ///
+    /// # Panics
+    ///
+    /// Panics if the new end of the string does not lie on a [`char`] boundary.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::new("Hello, world!");
+    /// s.truncate(5);
+    /// assert_eq!(s, "Hello");
+    /// ```
+    pub fn truncate(&mut self, new_len: usize) {
+        self.as_mut_compact_string().truncate(new_len)
+    }
+
+    /// Converts a [`CompactString`] to a raw pointer.
+    #[inline]
+    pub fn as_ptr(&self) -> *const u8 {
+        self.as_ref_compact_string().as_ptr()
+    }
+
+    /// Converts a mutable [`CompactString`] to a raw pointer.
+    #[inline]
+    pub fn as_mut_ptr(&mut self) -> *mut u8 {
+        self.as_mut_compact_string().as_mut_ptr()
+    }
+
+    /// Insert string character at an index.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::new("Hello!");
+    /// s.insert_str(5, ", world");
+    /// assert_eq!(s, "Hello, world!");
+    /// ```
+    pub fn insert_str(&mut self, idx: usize, string: &str) {
+        self.as_mut_compact_string().insert_str(idx, string)
+    }
+
+    /// Insert a character at an index.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::new("Hello world!");
+    /// s.insert(5, ',');
+    /// assert_eq!(s, "Hello, world!");
+    /// ```
+    pub fn insert(&mut self, idx: usize, ch: char) {
+        self.insert_str(idx, ch.encode_utf8(&mut [0; 4]));
+    }
+
+    /// Reduces the length of the [`CompactString`] to zero.
+    ///
+    /// Calling this function does not change the capacity of the [`CompactString`].
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::new("Rust is the most loved language on Stackoverflow!");
+    /// assert_eq!(s.capacity(), 49);
+    ///
+    /// s.clear();
+    ///
+    /// assert_eq!(s, "");
+    /// assert_eq!(s.capacity(), 49);
+    /// ```
+    pub fn clear(&mut self) {
+        self.as_mut_compact_string().clear()
+    }
+
+    /// Split the [`CompactString`] into at the given byte index.
+    ///
+    /// Calling this function does not change the capacity of the [`CompactString`], unless the
+    /// [`CompactString`] is backed by a `&'static str`.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `at` does not lie on a [`char`] boundary.
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::const_new("Hello, world!");
+    /// let w = s.split_off(5);
+    ///
+    /// assert_eq!(w, ", world!");
+    /// assert_eq!(s, "Hello");
+    /// ```
+    pub fn split_off(&mut self, at: usize) -> Self {
+        self.as_mut_compact_string().split_off(at).into()
+    }
+
+    /// Remove a range from the [`CompactString`], and return it as an iterator.
+    ///
+    /// Calling this function does not change the capacity of the [`CompactString`].
+    ///
+    /// # Panics
+    ///
+    /// Panics if the start or end of the range does not lie on a [`char`] boundary.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::new("Hello, world!");
+    ///
+    /// let mut d = s.drain(5..12);
+    /// assert_eq!(d.next(), Some(','));   // iterate over the extracted data
+    /// assert_eq!(d.as_str(), " world"); // or get the whole data as &str
+    ///
+    /// // The iterator keeps a reference to `s`, so you have to drop() the iterator,
+    /// // before you can access `s` again.
+    /// drop(d);
+    /// assert_eq!(s, "Hello!");
+    /// ```
+    pub fn drain(&mut self, range: impl RangeBounds<usize>) -> Drain<'_> {
+        self.as_mut_compact_string().drain(range)
+    }
+
+    /// Shrinks the capacity of this [`CompactString`] with a lower bound.
+    ///
+    /// The resulting capactity is never less than the size of 3×[`usize`],
+    /// i.e. the capacity than can be inlined.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::with_capacity(100);
+    /// assert_eq!(s.capacity(), 100);
+    ///
+    /// // if the capacity was already bigger than the argument, the call is a no-op
+    /// s.shrink_to(100);
+    /// assert_eq!(s.capacity(), 100);
+    ///
+    /// s.shrink_to(50);
+    /// assert_eq!(s.capacity(), 50);
+    ///
+    /// // if the string can be inlined, it is
+    /// s.shrink_to(10);
+    /// assert_eq!(s.capacity(), 3 * std::mem::size_of::<usize>());
+    /// ```
+    #[inline]
+    pub fn shrink_to(&mut self, min_capacity: usize) {
+        self.as_mut_compact_string().shrink_to(min_capacity)
+    }
+
+    /// Shrinks the capacity of this [`CompactString`] to match its length.
+    ///
+    /// The resulting capactity is never less than the size of 3×[`usize`],
+    /// i.e. the capacity than can be inlined.
+    ///
+    /// This method is effectively the same as calling [`string.shrink_to(0)`].
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::from("This is a string with more than 24 characters.");
+    ///
+    /// s.reserve(100);
+    /// assert!(s.capacity() >= 100);
+    ///
+    ///  s.shrink_to_fit();
+    /// assert_eq!(s.len(), s.capacity());
+    /// ```
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::from("short string");
+    ///
+    /// s.reserve(100);
+    /// assert!(s.capacity() >= 100);
+    ///
+    /// s.shrink_to_fit();
+    /// assert_eq!(s.capacity(), 3 * std::mem::size_of::<usize>());
+    /// ```
+    #[inline]
+    pub fn shrink_to_fit(&mut self) {
+        self.as_mut_compact_string().shrink_to_fit()
+    }
+
+    /// Retains only the characters specified by the predicate.
+    ///
+    /// The method iterates over the characters in the string and calls the `predicate`.
+    ///
+    /// If the `predicate` returns `false`, then the character gets removed.
+    /// If the `predicate` returns `true`, then the character is kept.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let mut s = CompactString::from("äb𝄞d€");
+    ///
+    /// let keep = [false, true, true, false, true];
+    /// let mut iter = keep.iter();
+    /// s.retain(|_| *iter.next().unwrap());
+    ///
+    /// assert_eq!(s, "b𝄞€");
+    /// ```
+    pub fn retain(&mut self, predicate: impl FnMut(char) -> bool) {
+        self.as_mut_compact_string().retain(predicate)
+    }
+
+    /// Decode a bytes slice as UTF-8 string, replacing any illegal codepoints
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let chess_knight = b"\xf0\x9f\xa8\x84";
+    ///
+    /// assert_eq!(
+    ///     "🨄",
+    ///     CompactString::from_utf8_lossy(chess_knight),
+    /// );
+    ///
+    /// // For valid UTF-8 slices, this is the same as:
+    /// assert_eq!(
+    ///     "🨄",
+    ///     CompactString::new(std::str::from_utf8(chess_knight).unwrap()),
+    /// );
+    /// ```
+    ///
+    /// Incorrect bytes:
+    ///
+    /// ```
+    /// # use compact_str::CompactString;
+    /// let broken = b"\xf0\x9f\xc8\x84";
+    ///
+    /// assert_eq!(
+    ///     "�Ȅ",
+    ///     CompactString::from_utf8_lossy(broken),
+    /// );
+    ///
+    /// // For invalid UTF-8 slices, this is an optimized implemented for:
+    /// assert_eq!(
+    ///     "�Ȅ",
+    ///     CompactString::from(String::from_utf8_lossy(broken)),
+    /// );
+    /// ```
+    pub fn from_utf8_lossy(v: &[u8]) -> Self {
+        String::from_utf8_lossy(v).into()
+    }
+
     /// Convert the [`CompactString`] into a [`String`].
     ///
     /// # Examples
@@ -459,8 +940,97 @@ impl<'a> CompactCowStr<'a> {
         self.0.into_string()
     }
 
-    fn into_compact_string(self) -> CompactString {
-        self.into()
+    /// Convert a [`String`] into a [`CompactString`] _without inlining_.
+    ///
+    /// Note: You probably don't need to use this method, instead you should use `From<String>`
+    /// which is implemented for [`CompactString`].
+    ///
+    /// This method exists incase your code is very sensitive to memory allocations. Normally when
+    /// converting a [`String`] to a [`CompactString`] we'll inline short strings onto the stack.
+    /// But this results in [`Drop`]-ing the original [`String`], which causes memory it owned on
+    /// the heap to be deallocated. Instead when using this method, we always reuse the buffer that
+    /// was previously owned by the [`String`], so no trips to the allocator are needed.
+    ///
+    /// # Examples
+    ///
+    /// ### Short Strings
+    /// ```
+    /// use compact_str::CompactString;
+    ///
+    /// let short = "hello world".to_string();
+    /// let c_heap = CompactString::from_string_buffer(short);
+    ///
+    /// // using CompactString::from_string_buffer, we'll re-use the String's underlying buffer
+    /// assert!(c_heap.is_heap_allocated());
+    ///
+    /// // note: when Clone-ing a short heap allocated string, we'll eagerly inline at that point
+    /// let c_inline = c_heap.clone();
+    /// assert!(!c_inline.is_heap_allocated());
+    ///
+    /// assert_eq!(c_heap, c_inline);
+    /// ```
+    ///
+    /// ### Longer Strings
+    /// ```
+    /// use compact_str::CompactString;
+    ///
+    /// let x = "longer string that will be on the heap".to_string();
+    /// let c1 = CompactString::from(x);
+    ///
+    /// let y = "longer string that will be on the heap".to_string();
+    /// let c2 = CompactString::from_string_buffer(y);
+    ///
+    /// // for longer strings, we re-use the underlying String's buffer in both cases
+    /// assert!(c1.is_heap_allocated());
+    /// assert!(c2.is_heap_allocated());
+    /// ```
+    ///
+    /// ### Buffer Re-use
+    /// ```
+    /// use compact_str::CompactString;
+    ///
+    /// let og = "hello world".to_string();
+    /// let og_addr = og.as_ptr();
+    ///
+    /// let mut c = CompactString::from_string_buffer(og);
+    /// let ex_addr = c.as_ptr();
+    ///
+    /// // When converting to/from String and CompactString with from_string_buffer we always re-use
+    /// // the same underlying allocated memory/buffer
+    /// assert_eq!(og_addr, ex_addr);
+    ///
+    /// let long = "this is a long string that will be on the heap".to_string();
+    /// let long_addr = long.as_ptr();
+    ///
+    /// let mut long_c = CompactString::from(long);
+    /// let long_ex_addr = long_c.as_ptr();
+    ///
+    /// // When converting to/from String and CompactString with From<String>, we'll also re-use the
+    /// // underlying buffer, if the string is long, otherwise when converting to CompactString we
+    /// // eagerly inline
+    /// assert_eq!(long_addr, long_ex_addr);
+    /// ```
+    #[inline]
+    #[track_caller]
+    pub fn from_string_buffer(s: String) -> Self {
+        CompactString::from_string_buffer(s).into()
+    }
+
+    #[inline]
+    fn into_compact_string(mut self) -> CompactString {
+        self.0.make_owned();
+        unsafe { std::mem::transmute(self) }
+    }
+
+    #[inline]
+    fn as_ref_compact_string(&self) -> &CompactString {
+        unsafe { std::mem::transmute(self) }
+    }
+
+    #[inline]
+    fn as_mut_compact_string(&mut self) -> &mut CompactString {
+        self.0.make_owned();
+        unsafe { std::mem::transmute(self) }
     }
 }
 
@@ -490,9 +1060,8 @@ impl<'a> From<&'a CompactString> for CompactCowStr<'a> {
 
 impl<'a> From<CompactCowStr<'a>> for CompactString {
     #[inline]
-    fn from(mut value: CompactCowStr<'a>) -> Self {
-        value.0.owned();
-        unsafe { std::mem::transmute(value) }
+    fn from(value: CompactCowStr<'a>) -> Self {
+        value.into_compact_string()
     }
 }
 
